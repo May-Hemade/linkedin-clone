75075955c461799c44fd13951d25a408448d47a4
May-Hemade <may.hemade1993@gmail.com>
2022-01-17 11:59:23 +0200

    added about

---
+++ src/Components /About.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Pen } from "react-bootstrap-icons"
+import { Container } from "react-bootstrap"
+
+export default function About() {
+  return (
+    <div>
+      <Container>
+        <div>
+          <h2>About</h2>
+          <Pen />
+        </div>
+        <div>
+          Being a business developer for multiple businesses has allowed me to
+          gain domain knowledge in multiple sectors. I have recently shifted my
+          focus to the IT industry - Healthcare in particular. I have conceived
+          a product called Know.me which gives health facilities, companies, and
+          individuals a higher level of safety by providing easy access to vital
+          information in critical situations.
+        </div>
+      </Container>
+    </div>
+  )
+}
