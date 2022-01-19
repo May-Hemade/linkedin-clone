87714a820ccd06778fa3abef4beb70d96a283054
May-Hemade <may.hemade1993@gmail.com>
2022-01-19 15:30:52 +0200

    fetch and display home posts

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,69 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Col, Row, Container } from "react-bootstrap"
+import { CircleLoader, GridLoader } from "react-spinners"
 import Footer from "../Footer"
 import RightSideBar from "../RightSideBar"
+import { css } from "@emotion/react"
+import Post from "./Post"
+
+const loadingStyle = css`
+  display: block;
+  margin: 0 auto;
+  border-color: #70b5f9;
+`
 
 export default function Home() {
+  const [posts, setPosts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  const getPosts = async () => {
+    setIsLoading(true)
+
+    const response = await fetch(
+      "https://striveschool-api.herokuapp.com/api/posts",
+      {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWU1MzE4ODczZDVjYjAwMTUzOTVhOWYiLCJpYXQiOjE2NDI0MTAzNzYsImV4cCI6MTY0MzYxOTk3Nn0.qDjDBTYnXI7X3Y3eWLOaKSMaVRFITbDsAwrjjesIIMc",
+        },
+      }
+    )
+
+    if (response.ok) {
+      setPosts(await response.json())
+      setIsLoading(false)
+    } else {
+      setIsLoading(false)
+      setHasError(true)
+    }
+  }
+
   return (
     <div>
       <Container className="mt-4 pt-32">
         <Row>
-          <Col></Col>
+          <Col md={2}></Col>
+
+          <Col>
+            <GridLoader
+              size={10}
+              loading={isLoading}
+              color="#70b5f9"
+              css={loadingStyle}
+            />
+
+            {!isLoading &&
+              !hasError &&
+              posts
+                .slice(0, 100)
+                .map((post) => <Post key={post._id} post={post} />)}
+          </Col>
           <Col md={3}></Col>
         </Row>
       </Container>
---
+++ src/components/home/Post.jsx
@@ -0,0 +1,69 @@
+import { SendOutlined, ThumbUp, ThumbUpOutlined } from "@material-ui/icons"
+import React from "react"
+import { Button } from "react-bootstrap"
+import { FaRegCommentDots, FaShare } from "react-icons/fa"
+import "./post.css"
+
+export default function Post({ post }) {
+  const getDateString = () => {
+    return "1d"
+  }
+
+  return (
+    <div className="linkedin-post-container linkedin-card">
+      <div className="linkedin-post-header d-flex align-items-center">
+        <img src={post.user.image} className="linkedin-user-image" />
+
+        <div>
+          <div className="linkedin-user-name">
+            {post.user.name} {post.user.surname}
+          </div>
+          <div className="linkedin-user-title">{post.user.title}</div>
+          <div className="linkedin-post-date">{getDateString()}</div>
+        </div>
+      </div>
+      <div className="linkedin-post-body">
+        <p>{post.text}</p>
+      </div>
+      <div className="linkedin-post-buttons linkedin-divider-top">
+        <Button className="linkedin-post-button text-center">
+          <ThumbUpOutlined className="flip" />
+          <span className="ml-1">Like</span>
+        </Button>
+        <Button className="linkedin-post-button">
+          <FaRegCommentDots /> <span className="ml-1">Comment</span>
+        </Button>
+        <Button className="linkedin-post-button">
+          <FaShare /> <span className="ml-1">Share</span>
+        </Button>
+        <Button className="linkedin-post-button">
+          <SendOutlined className="rotate-icon" />{" "}
+          <span className="ml-1">Send</span>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+// {
+//     "_id": "5fc6ba2a0626b5001729ee1d",
+//     "text": "Hello Wor.. ehm.. Strivers! ;D",
+//     "username": "StefanoMiC",
+//     "user": {
+//         "_id": "5fc4c2beed266800170ea3d4",
+//         "name": "Stefano",
+//         "surname": "Miceli",
+//         "email": "mic@nucleode.com",
+//         "username": "StefanoMiC",
+//         "title": "Front-End Developer & UX Designer",
+//         "bio": "I'm very passionate about Graphic Design and any Visual Art. Never underestimate the power of a usable interface before a pretty one! \n\nI also love Photography and the developing process either in a dark room or the digital one.",
+//         "area": "Udine, Friuli Venezia Giulia, Italy",
+//         "image": "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+//         "createdAt": "2020-11-30T10:00:31.279Z",
+//         "updatedAt": "2020-11-30T10:00:31.279Z",
+//         "__v": 0
+//     },
+//     "createdAt": "2020-12-01T21:48:26.995Z",
+//     "updatedAt": "2020-12-01T21:50:50.616Z",
+//     "__v": 0
+// },
