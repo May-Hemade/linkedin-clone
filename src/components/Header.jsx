07c08796c9f3195ca7fc2eedc56ce5700d0ed0d2
Anudeep <36949642+anucherukuri@users.noreply.github.com>
2022-01-19 12:58:41 +0000

    NavBar updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import './Header.css';
 import 'react-bootstrap-icons'
+import {Nav,Dropdown, DropdownButton} from 'react-bootstrap'
+
+
 import SearchIcon from '@material-ui/icons/Search'
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@material-ui/icons/Home'
@@ -8,15 +12,18 @@ import SupervisorIcon from '@material-ui/icons/SupervisorAccount'
 import ChatIcon from '@material-ui/icons/Chat'
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
 import NotificationsIcon from '@material-ui/icons/Notifications'
-import GridOnIcon from '@material-ui/icons/GridOn';
+import { BsGrid3X3GapFill } from "react-icons/bs";
+
 
 
 
 const Header = () => {
+    
+  
     return(
         <div className="header">
                 <div className="header_left">
-                    <img src="./Assets/Linkedin-logo.png" alt=""/>
+                    <img src="./Assets/Linkedin-logo.png" className="Logo" alt="logo"/>
                         <div className="header_search">
                             <SearchIcon/>
                             <input type= "text" className="search_Bar_input"/>
@@ -24,16 +31,57 @@ const Header = () => {
                 </div>
 
                 <div className="header_right">
-                    <HeaderOption Icon={HomeIcon} title='Home'/>
-                    <HeaderOption Icon={SupervisorIcon} title='My Network'/>
-                    <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
-                    <HeaderOption Icon={ChatIcon} title='Messaging'/>
-                    <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
-                    <HeaderOption avatar='./Assets/Profile-picture.jpg' title='Me'/>
-                    <HeaderOption Icon= {GridOnIcon} title='Work'/>
+                   
+                <Nav>     
+                    <Nav.Item>
+                        <Nav.Link href="/home">
+                            <HeaderOption Icon={HomeIcon} title='Home'/>
+                        </Nav.Link>
+                    </Nav.Item>           
+                    <Nav.Item>
+                        <Nav.Link href="/home">
+                            <HeaderOption Icon={SupervisorIcon} title='My Network'/>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">
+                            <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">
+                            <HeaderOption Icon={ChatIcon} title='Messaging'/>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">
+                            <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">
+                            <HeaderOption avatar='./Assets/Profile-picture.jpg' title='Me'/>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">
+                            <HeaderOption Icon= {BsGrid3X3GapFill} title='Work'/>
+                        </Nav.Link>
+                    </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                    Get Hired Faster, Try Premium Free
+                    </Nav.Link>
+                </Nav.Item>
+                </Nav>
+
+
                 </div>
         </div>
     )
 }
 
+
+
 export default Header
+
