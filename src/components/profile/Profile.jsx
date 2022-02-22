7206ed8b00d7c5ad489d9be8221ee151e87f0680
Carlos Gozalbes <carlosfgmendoza@gmail.com>
2022-02-22 15:07:50 +0100

    working on profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -9,9 +9,54 @@ import CentralAvatar from "../CentralAvatar"
 import Dashboard from "./Dashboard"
 import Footer from "../Footer"
 import RightSideBar from "../RightSideBar"
-import Header from "../Header"
+
+import { useState } from "react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { GridLoader } from "react-spinners";
+
+const loadingStyle = css`
+  display: block;
+  margin: 0 auto;
+  border-color: #70b5f9;
+`;
+
 
 function Profile() {
+  const {profileId} = useParams()
+  const [profile,setProfile]=useState(null)
+  const [loading,setLoading]=useState(true)
+  
+  const fetchProfile = async () => {
+    try {
+      let response = await fetch(`http://localhost:3001/profile/${profileId}`);
+      if (response.ok) {
+        let data = await response.json();
+        console.log(data);
+        setProfile(data);
+        setLoading(false);
+      } else {
+        console.log(`something went wrong`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  useEffect(()=>{
+    fetchProfile()
+  },[])
+  
+  if(loading) {
+    return (
+      <GridLoader
+        size={10}
+        loading={isLoading}
+        color="#70b5f9"
+        css={loadingStyle}
+      />
+    );
+  }
   return (
     <div>
       <Container className="mt-4 pt-32">
